7ac4aeea63fc5b265d5127289cc9d717fcbf3125
Tproc <tom@tproc.net>
2024-07-20 19:22:56 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using SimpleCrypto;
+
+/// <summary>
+///     Generate password hash and salt
+/// </summary>
+/// <param name="password">plaintext password</param>
+/// <param name="saltStr">salt of password</param>
+/// <returns></returns>
+/// 
+Console.WriteLine("IW4M Pass:");
+string pass =Console.ReadLine();
+Console.WriteLine(Hash("Password"));
+Console.WriteLine("\nTop - Password\nBottom - PasswordSalt");
+Console.WriteLine("Any Button to exit");
+Console.ReadLine();
+
+static string[] Hash(string password, string saltStr = null)
+{
+    string hash;
+    string salt;
+    var CryptoSvc = new PBKDF2();
+
+    // generate new hash 
+    if (saltStr == null)
+    {
+        hash = CryptoSvc.Compute(password);
+        Console.WriteLine(hash);
+        salt = CryptoSvc.Salt;
+        Console.WriteLine(salt);
+        return new[] {
+      hash,
+      salt
+    };
+    }
+
+    hash = CryptoSvc.Compute(password, saltStr);
+    return new[] {
+    hash,
+    ""
+
+  };
+
+    
+}
